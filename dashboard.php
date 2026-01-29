40796be38b01eb72f83e6acb39fb1a1255e16794
RyanFerry-coder <ferryhoneycomb09@gmail.com>
2026-01-29 20:14:42 +0700

    Enhance dashboard with user information display
    Updated welcome message to include user details.

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,11 @@ if (!isset($_SESSION['login'])) {
     header("Location: index.php");
     exit;
 }
-echo "Selamat Anda Berhasil Login<br>";
 echo "Welcome to My Website<br>";
+echo "FASILKOM<br>";
+echo "Nama : Ryan Ferry Ramadhan<br>";
+echo "NPM : D1A251001<br>";
+echo "Kelas : 1C Karyawan <br>";
 echo $_SESSION['session_username'];
+
 ?>
